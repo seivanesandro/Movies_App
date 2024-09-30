593afe06b7cd18ec7aad38caf1a7fc359a568047
Sandro Seivane <seivanesandro@gmail.com>
2024-09-30 13:09:27 +0100

    prepare the OMDb website API URL

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+//const apiKey = process.env.REACT_APP_API_KEY;
+//const apiId = process.env.REACT_APP_API_ID;
+//const apiUrl = process.env.REACT_APP_API_URL;
+
 const App = () => {
   return (
     <div className="App">
-      my movies app
+      <h1>My Movies App</h1>
     </div>
   )
 }
